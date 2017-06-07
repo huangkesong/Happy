a7025da6d11c34964174a8510bf6dd8fa751820d
Hixon <40071390@qq.com>
2017-06-07 18:25:23 +0800

    drag.html And drag.js update

--- drag.js
+++ drag.js
@@ -1,7 +1,10 @@
 //函数声明
 function deap(obj){
+    var disX = 0;
+    var disY = 0;
     var oDiv = document.getElementById(obj);
     oDiv.onmousedown = function(){
-        console.log("我被触发了");
+        console.log("我现在做了一个事件操作！！！");
+        console.log("哈哈 我又变动了！！！");
     }
 }
