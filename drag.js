75f63b475d9d55b4189bdab9cc8dd12bd8a9914b
Hixon <40071390@qq.com>
2017-06-12 11:44:06 +0800

    success drag

--- drag.js
+++ drag.js
@@ -1,10 +1,20 @@
 //函数声明
-function deap(obj){
+function drag(obj){
     var disX = 0;
     var disY = 0;
     var oDiv = document.getElementById(obj);
-    oDiv.onmousedown = function(){
-        console.log("我现在做了一个事件操作！！！");
-        console.log("哈哈 我又变动了！！！");
+    oDiv.onmousedown = function(ev){
+       disX = ev.pageX - oDiv.offsetLeft;
+       disY = ev.pageY - oDiv.offsetTop;
+        console.log(disX,disY);
+        document.onmousemove = function(ev){
+            oDiv.style.left = ev.pageX - disX +'px';
+            oDiv.style.top = ev.pageX - disY +'px';
+        };
+        document.onmouseup = function(ev){
+          document.onmousemove = null;
+          document.onmouseup = null;
+        };
     }
+    return false;
 }
